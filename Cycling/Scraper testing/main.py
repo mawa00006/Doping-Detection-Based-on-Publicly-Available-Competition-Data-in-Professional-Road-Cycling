2e813b0dd91e4fe6fc57e6dde9cbdd835aeecea6
Mattes <mattes.warning@gmx.de>
2022-05-25 20:43:01 +0200

    rewriting main/scraping

--- Cycling/Scraper testing/main.py
+++ Cycling/Scraper testing/main.py
@@ -1,22 +1,11 @@
 import numpy as np
 
-from scraping import get_rider_teams
-from scraping import get_rider_years
-from scraping import get_rider_details
-from scraping import scrape_riders_from_team
+
 from scraping import scrape_one_day_results
-from scraping import scrape_race_startlist
-from scraping import scrape_stage_race_stage_results
 from scraping import scrape_race_information
-from scraping import scrape_races_for_year
-from scraping import scrape_stage_race_all_stage_results
-from scraping import scrape_stage_race_overview_competing_teams
-from scraping import scrape_stage_race_overview_stages
-from scraping import scrape_stage_race_overview_top_competitors
-from scraping import scrape_tour_races_for_year
-from scraping import scrape_teams_for_year
-from scraping import scrape_rider_all_results
-from scraping import scrape_one_day_race_information
+from scraping_new import scrape_races_for_year
+from scraping_new import scrape_stage_race_all_stage_results
+
 
 import pandas as pd
 import time
@@ -29,16 +18,16 @@ def main():
 
     df_stages, df_oneday, df_TTT, df_ITT = prepare_dataframes()
 
-    stage_results = scrape_one_day_results('https://www.procyclingstats.com/race/gp-samyn/2020/result')
+    #stage_results = scrape_one_day_results('https://www.procyclingstats.com/race/gp-samyn/2020/result')
 
 
-    # all years from which to scrape available race data
+    # all years for to scrape available race data
     years = [2022]
     for year in years:
 
         # get all professional races held in a given year
         races = scrape_races_for_year(year)
-        races= races[races["cancelled"]== False]
+
         # for each race scrape all data
         for race in races.itertuples():
 
@@ -47,15 +36,15 @@ def main():
             stage_race = getattr(race, 'stage_race')
 
             race_info_df = pd.DataFrame(race).T
-            race_info_df = race_info_df.drop(race_info_df.columns[[0,1, 7]], axis = 1)
-            race_info_df.columns = ["race_name","stage_race","race_class","race_country_code",
-                                    "cancelled","tour", 'tour_code']
+            #race_info_df = race_info_df.drop(race_info_df.columns[[0,1, 7]], axis = 1)
+            #race_info_df.columns = ["race_name","stage_race","race_class","race_country_code",
+            #                        "cancelled","tour", 'tour_code']
 
             # get race page url
             url = getattr(race, 'race_url')
             # scrape information and final result of each stage in a stage race
             if stage_race:
-                stage_results, stages = scrape_stage_race_all_stage_results(url+'/overview', collecting =1)
+                stage_results, stages = scrape_stage_race_all_stage_results(url+'/overview')
 
                 # add race/stage information to each performance point
                 for i in range(len(stage_results)):
---
+++ Cycling/Scraper testing/scraping_new.py
@@ -0,0 +1,503 @@
+from bs4 import BeautifulSoup
+import requests
+from requests_html import HTMLSession # to remove
+from datetime import timedelta
+import pandas as pd
+import numpy as np
+import re
+from datetime import datetime
+
+def scrape_races_for_year(year=2020) -> pd.DataFrame:
+    """
+    SUMMARY
+    get details of all races which occurred in a given year (for all available tours).
+    E.G. https://www.procyclingstats.com/races.php?year=2020
+    PARAMETERS
+    year (int): year to get races for (default=2020)
+    OUTPUT
+    pandas.DataFrame: fetched data includes:
+                        "stage_race" (bool) whether race is a stage race of not
+                        "race_url" (str) full url to race overview page
+                        "tour" (str) name of tour race occured in
+                        "tour_code" (int) PCS code for tour
+    """
+    years=get_available_tours_for_year(year)
+
+    # prepare data frame
+    df=pd.DataFrame()
+    i = 0
+    for key,value in years.items():
+        # drop all junior races
+        if value in [15,16,17]:
+            continue
+
+        print("{}             ".format(key),end="\r")
+        year_race_series=scrape_tour_races_for_year(year=year,tour_code=value)
+        year_race_series["tour"]=key
+        year_race_series["tour_code"]=value
+        df=pd.concat([df,year_race_series],ignore_index=True)
+
+    #drop all cancelled races
+    df = df[df["cancelled"]== False]
+    df.drop("cancelled", axis = 1, inplace= True)
+
+    return df
+
+def get_available_tours_for_year(year=2020) -> {str:int}:
+    """
+    SUMMARY
+    get details for all tours which occured in a given year.
+    E.G. https://www.procyclingstats.com/races.php?year=2020
+    PARAMETERS
+    year (int): year to get tours for (default=2020)
+    OUTPUT
+    {str:int}: dictionary from `tour_name` to `tour_code`
+    """
+    # format url
+    url="https://www.procyclingstats.com/races.php?year={}".format(year)
+
+    # fetch data
+    session=HTMLSession()
+    response=session.get(url)
+    response.html.render()
+    soup=BeautifulSoup(response.html.html,"lxml")
+
+    # isolate input field
+    select_field=soup.find("select",{"name":"circuit"})
+    select_field_options=select_field.find_all("option")
+
+    # prepare dict
+    tours={}
+
+    # fill dict
+    for option in select_field_options:
+
+        if len(option["value"]) == 0:
+            continue
+        tours[option.text]=int(option["value"])
+
+    return tours
+
+def scrape_tour_races_for_year(year=2020,tour_code=1) -> pd.DataFrame:
+    """
+    SUMMARY
+    get details for all races which occured in a given tour, in a given year
+    E.G. https://www.procyclingstats.com/races.php?year=2020&circuit=1
+    PARAMETERS
+    year (int): year to get races from (default=2020)
+    tour_code (int): PCS code for tour to get details of (default=1)
+    OUTPUT
+    pandas.DataFrame: fetched data includes:
+                        "stage_race" (bool) whether race is a stage race of not
+                        "race_class" (str) classification of race
+                        "cancelled" (bool) whether race was/is cancelled
+                        "race_url" (str) full url to race overview page
+    """
+    # format url
+    url="https://www.procyclingstats.com/races.php?year={}&circuit={}".format(year,tour_code)
+
+    # fetch data
+    session=HTMLSession()
+    response=session.get(url)
+    response.html.render()
+    soup=BeautifulSoup(response.html.html,"lxml")
+
+    table_div=soup.find("table",{"class":"basic"})
+    table_body=table_div.find("tbody")
+    table_rows=table_body.find_all("tr")
+
+    df=pd.DataFrame(columns=["stage_race","cancelled","race_url"])
+
+    for row in table_rows:
+        series=pd.DataFrame(parse_tour_races_for_year_row(row)).T
+        df=pd.concat([df,series],axis=0,ignore_index=True)
+
+    return df
+
+def parse_tour_races_for_year_row(row) -> pd.Series:
+    """
+    SUMMARY
+    parse details from row of table of races in a given year & tour
+    used by Scraper.scrape_tour_races_for_year
+    PARAMETERS
+    row (bs4.element.Tag): row from table
+    OUTPUT
+    pandas.Series: fetched data includes:
+                        "stage_race" (bool) whether race is a stage race of not
+                        "cancelled" (bool) whether race was/is cancelled
+                        "race_url" (str) full url to race overview page
+    """
+    series={}
+
+    row_details=row.find_all("td")
+
+    # extract details
+    try:
+        series["cancelled"]= ("striked" in row["class"])
+    except:
+        series["cancelled"]= False
+    series["stage_race"]=("-" in row_details[0].text)
+    series["race_url"]="https://www.procyclingstats.com/"+row_details[2].find("a")["href"]
+
+    return pd.Series(series)
+
+
+def scrape_one_day_race_information(url:str):
+    """
+    SUMMARY
+    scrape race information from a one day race url
+    E.G. https://www.procyclingstats.com/race/milano-sanremo/2014
+    PARAMETERS:
+    url (str): url for a one day race
+    OUTPUT
+    pandas.Dataframe: fetched data includes
+                    "date" () date of the race
+                    "race_name" (str) name of race
+                    "race_class" (str) classification of race
+                    "race_ranking" (int) ranking of race
+                    "race_country_code" (str) code for host country
+                    "start_location" (str) name of start town
+                    "end_location" (str) name of finish town
+                    "pcs_points_scale" (str) name of points scale being used
+                    "profile" (str) code for profile of race
+                    "distance" (int) distance of stage in km
+                    "vertical_meters" (int) vertically climbed distance of stage in km
+                    "startlist_quality-score" (int) quality of startlist
+
+    """
+    # start session
+    session = HTMLSession()
+    response = session.get(url)
+    response.html.render()
+    soup = BeautifulSoup(response.html.html, "lxml")
+
+    #series to fill in
+    series = pd.Series()
+
+    #isolate main info
+    main = soup.find("div",{"class":"main"})
+
+    # isolate race information table
+    infolist = soup.find("ul", {"class": "infolist"}).find_all("div")
+
+    #scrape data
+    series["date"] = datetime.strptime(infolist[1].text, '%d %B %Y').date()
+    series["race_name"] = main.find("h1").text
+    series["race_class"] = main.find_all("font")[1].text
+    series["race_ranking"] = infolist[23].text
+    series["race_country_code"] = main.find("span",{"class":"flag"})["class"][1]
+    series["start_location"] = infolist[19].text
+    series["end_location"] = infolist[21].text
+    series["pcs_points_scale"] = infolist[11].text
+    series["profile"] = infolist[13].contents[0].attrs['class'][2]
+    series["distance"] = infolist[9].text
+    series["vertical_meters"] = infolist[17].text
+    series["startlist_quality_score"] = infolist[25].text
+
+    return pd.Series(series)
+
+def scrape_stage_race_names_urls(url:str) -> pd.DataFrame:
+    """
+    SUMMARY
+    get details for stages in a stage race from it's overview page
+    E.G. https://www.procyclingstats.com/race/tour-de-france/2019/overview
+    PARAMETERS
+    url (str): url for a race's overview page
+    OUTPUT
+    type: description
+    pandas.DataFrame: fetched data includes
+                        "stage_name" (str) name of stage (`stage #` or `REST DAY`)
+                        "stage_url" (str) full url to stage's detail page
+    """
+    # fetch data
+    session=HTMLSession()
+    response=session.get(url)
+    response.html.render()
+    soup=BeautifulSoup(response.html.html,"lxml")
+
+    # isolate desired list
+    left_div=soup.find_all("div",{"class":"mt20"})[1]
+    stage_list=left_div.find_all("ul")
+
+    # get list items
+    stage_list_items=left_div.find_all("li")
+
+    # prepare data frame
+    df=pd.DataFrame(columns=["stage_name","stage_url", "TTT"])
+
+    # fill data frame
+
+    for list_item in stage_list_items:
+        pattern = r'[0-9]'
+        test = re.sub(pattern, '',list_item.text)
+
+        if  test!= '/Restday': series=parse_stage_list_item(list_item) # not a rest day
+        else: series=pd.Series({"stage_name":"REST DAY"}) # is a rest day
+        df=df.append(series,ignore_index=True)
+    df =df[(df["stage_name"] != "REST DAY") & (df["TTT"] != True)]
+    df.drop("TTT", axis=1, inplace=True)
+    return df
+
+def parse_stage_list_item(list_item) -> pd.Series:
+    """
+    SUMMARY
+    get details about a single stage
+    USED by Scraper.scrape_stage_race_overview_stages
+    PARAMETERS
+    list_item (bs4.element.Tag): stage item from list of stages
+    OUTPUT
+    type: description
+    pandas.Series: fetched data includes
+                        "stage_url" (str) full url to stage's detail page
+                        "stage_name" (str) name of stage (`stage #` or `REST DAY`)
+                        "TTT" (boolean) Team Time Trial or not
+    """
+    series={}
+
+
+    # url
+    stage_details=list_item.find("a")
+    series["stage_url"]="https://www.procyclingstats.com/"+stage_details["href"]
+
+    # stage name
+    stage_detail_divs=list_item.find_all("div")
+    locations=stage_detail_divs[2].text.split("|")
+    series["stage_name"]=locations[0]
+
+    # Team time trial
+    if re.search("(TTT)",locations[0]):
+        series["TTT"]= True
+    else:
+        series["TTT"]= False
+
+    return pd.Series(series)
+
+
+
+def scrape_stage_race_all_stage_results(url:str) -> [pd.DataFrame]:
+    """
+    SUMMARY
+    get finishing results for each stage in a stage race.
+    E.G. https://www.procyclingstats.com/race/tour-de-france/2020/overview
+    PARAMETERS
+    url (str): full url to stage race overview
+    OUTPUT
+    type: description
+    list(pandas.DataFrame): one dataframe for results for each stage. each dataframe includes
+    """
+    stages=scrape_stage_race_names_urls(url)
+
+    results=[]
+
+
+    # scrape data for 'normal stages'
+    for stage in stages.itertuples():
+        stage_url = stage[2]
+        if stage_url[:4]!="http": stage_url="https://"+stage_url
+        print(stage_url)
+        stage_results_df=scrape_stage_race_stage_results(stage_url)
+        info_df = pd.DataFrame(scrape_one_day_race_information(stage_url)).T
+        info_df.insert(0, "stage_name", stage[1])
+        info_df = pd.concat([info_df]*stage_results_df.shape[0], ignore_index= True)
+        stage_results_df = pd.concat([stage_results_df, info_df], axis= 1)
+
+        results.append(stage_results_df)
+
+    return results
+
+def scrape_stage_race_stage_results(url:str) -> pd.DataFrame:
+    """
+    SUMMARY
+    get finish results for individual stage of a stage race
+    E.G. https://www.procyclingstats.com/race/tour-de-france/2020/stage-5
+    PARAMETERS
+    url (str): full url for a stage
+    OUTPUT
+    type: description
+    pandas.DataFrame: fetched data includes
+                        "stage_pos" (int) finish position of rider (`np.NaN` if rider didn't finish stage)
+                        "gc_pos" (int) rider's gc position after stage (`np.NaN` if rider didn't finish stage)
+                        "rider_age" (int) rider's age on day of stage
+                        "team_name" (str) name of rider's team
+                        "rider_name" (str) name of rider
+                        "rider_nationality_code" (str) PCS code for rider's nationality
+                        "uci_points" (int) number of uci points won by rider in stage
+                        "points" (int) number of PCS points won by rider in stage
+                        "striked" (int) result was stripped (doping?)
+    """
+    # start session
+    session=HTMLSession()
+    response=session.get(url)
+    response.html.render()
+    soup=BeautifulSoup(response.html.html,"lxml")
+    infos = soup.find('w30 right mg_rp10')
+    # isolate desired table
+    table=soup.find("table")
+    if (table is None): return None # results don't exist
+
+    results_table=table.find("tbody")
+    rows=results_table.find_all("tr")
+
+    # prepare data frame
+    df=pd.DataFrame(columns=["stage_pos","gc_pos","rider_age","team_name","rider_name",
+                             "rider_nationality_code","uci_points","points", "striked"])
+
+    # fill data frame
+    for row in rows:
+        series=parse_stage_race_stage_results_row(row)
+        df=df.append(series,ignore_index=True)
+
+    return df
+
+def parse_stage_race_stage_results_row(row) -> pd.Series:
+    """
+    SUMMARY
+    parse data from row of stage results table
+    USED by Scraper.scrape_stage_race_stage_results
+    PARAMETERS
+    row (bs4.element.Tag): row to extract details from
+    OUTPUT
+    pandas.Series: fetched data includes
+                        "stage_pos" (int) finish position of rider
+                        "gc_pos" (int) rider's gc position after stage
+                        "rider_age" (int) rider's age on day of stage
+                        "team_name" (str) name of rider's team
+                        "rider_name" (str) name of rider
+                        "rider_nationality_code" (str) PCS code for rider's nationality
+                        "uci_points" (int) number of uci points won by rider in stage
+                        "points" (int) number of PCS points won by rider in stage
+                        "striked" (int) result was stripped (doping?)
+    """
+    series={}
+    row_data=row.find_all("td")
+    striked = row.find_all("s")
+
+    # race details
+    stage_pos=row_data[0].text
+    gc_pos=row_data[1].text
+
+    try:
+        series["stage_pos"]=int(stage_pos) if (stage_pos not in ["DNF","OTL","DNS","DF", "DSQ"]) else np.NaN
+    except:
+        series["stage_pos"]= np.NaN
+
+    series["gc_pos"]=int(gc_pos) if (gc_pos!="") else np.NaN
+
+    # rider and team details
+    series["rider_age"]=row_data[6].text
+    series["team_name"]=row_data[7].text
+    series["rider_name"]=row_data[5].find('a').attrs['href'].split('/')[1]
+    series["rider_nationality_code"]=row_data[5].find("span",{"class":"flag"})["class"][-1]
+
+    # point results
+    uci_points=row_data[8].text
+    points=row_data[9].text
+    series["uci_points"]=int(uci_points) if (uci_points!="") else 0
+    series["points"]=int(points) if (points!="") else 0
+    if len(striked) != 0:
+        series["striked"]= 1
+    else:
+        series["striked"]= 0
+
+    return pd.Series(series)
+
+"""
+ONE DAY RACING
+"""
+
+def scrape_one_day_results(url:str) -> pd.DataFrame:
+    """
+    SUMMARY
+    get finish results for a one day race, from its results page
+    E.G. https://www.procyclingstats.com/race/gp-samyn/2020/result
+    PARAMETERS
+    url (str): full url for a one day race results page
+    OUTPUT
+    type: description
+    pandas.DataFrame: fetched data includes
+                        "striked" (boolean) true if performance was striked out
+                        "finish_pos" (int) finish position of rider (`np.NaN` if rider didn't finish stage)
+                        "bib_number" (int) rider's race number
+                        "rider_age" (int) rider's age on day of stage
+                        "team_name" (str) name of rider's team
+                        "rider_name" (str) name of rider
+                        "rider_nationality_code" (str) PCS code for rider's nationality
+                        "uci_points" (int) number of uci points won by rider in stage
+                        "points" (int) number of PCS points won by rider in stage
+                        "finish_time" (datetime.timedelta) time taken to complete stage (or time behind stage winner)
+    """
+    # start session
+    session=HTMLSession()
+    response=session.get(url)
+    response.html.render()
+    soup=BeautifulSoup(response.html.html,"lxml")
+
+    # isolate desired table
+    table=soup.find("table")
+    if (table is None): return None # results don't exist
+
+    results_table=table.find("tbody")
+    rows=results_table.find_all("tr")
+
+    # prepare data frame
+    df=pd.DataFrame(columns=["stage_pos","bib_number","rider_age","team_name","rider_name","rider_nationality_code","uci_points","points"])
+
+    # get race information
+    race_inf = scrape_one_day_race_information(url)
+
+    # fill data frame
+    for row in rows:
+        series=parse_one_day_results_row(row)
+        series = pd.DataFrame(pd.concat([race_inf, series], axis=0)).T
+        df=pd.concat([df,series],axis =0,ignore_index=True)
+
+    return df
+
+def parse_one_day_results_row(row) -> pd.Series:
+    """
+    SUMMARY
+    parse data from row of one-day results table
+    USED by Scraper.scrape_stage_race_stage_results
+    PARAMETERS
+    row (bs4.element.Tag): row to extract details from
+    OUTPUT
+    pandas.Series: fetched data includes
+                        "finish_pos" (int) finish position of rider (`np.NaN` if rider didn't finish stage)
+                        "bib_number" (int) rider's race number
+                        "rider_age" (int) rider's age on day of stage
+                        "team_name" (str) name of rider's team
+                        "rider_name" (str) name of rider
+                        "rider_nationality_code" (str) PCS code for rider's nationality
+                        "uci_points" (int) number of uci points won by rider in stage
+                        "points" (int) number of PCS points won by rider in stage
+                        "finish_time" (datetime.timedelta) time taken to complete stage (or time behind stage winner)
+    """
+    series={}
+    row_data=row.find_all("td")
+    striked = row.find_all("s")
+    # race details
+    finish_pos=row_data[0].text
+    series["stage_pos"]=int(finish_pos) if (finish_pos not in ["DF","DNF","OTL","DNS", "DSQ"]) else np.NaN
+    series["bib_number"]=int(row_data[1].text)
+
+    # rider and team details
+    series["team_name"]=row_data[5].text
+    series["rider_name"]=row_data[3].find('a').attrs['href'].split('/')[1]
+    series["rider_nationality_code"]=row_data[3].find("span",{"class":"flag"})["class"][-1]
+    series["rider_age"]=int(row_data[4].text)
+
+    # point results
+    uci_points=row_data[6].text
+    points=row_data[7].text
+    series["uci_points"]=int(uci_points) if (uci_points!="") else 0
+    series["points"]=int(points) if (points!="") else 0
+
+    #striked performance
+    if len(striked) != 0:
+        series["striked"]= 1
+    else:
+        series["striked"]= 0
+
+
+
+    return pd.Series(series)
