ed03868542ecce763cb7b003bf5915b71ec5e3af
Mattes <mattes.warning@gmx.de>
2022-05-27 17:25:18 +0200

    file management

--- Cycling/Wikipedia_scraping.py
+++
@@ -1,8 +0,0 @@
-import requests as rq
-from bs4 import BeautifulSoup as bs
-
-
-url = 'https://en.wikipedia.org/wiki/List_of_doping_cases_in_athletics'
-
-s = rq.Session()
-response = s.get(url, timeout= 10)
