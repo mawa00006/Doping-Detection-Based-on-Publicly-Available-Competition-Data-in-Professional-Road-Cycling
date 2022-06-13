5fe1acdde7c9b1a0f045edb019867acce5bb012d
Mattes <mattes.warning@gmx.de>
2022-06-13 21:51:32 +0200

    scraped doping incidents from dopeology.com close #21

--- Cycling/Data Scraping/scrape_dopeology.py
+++ Cycling/Data Scraping/scrape_dopeology.py
@@ -1,37 +1,47 @@
 from bs4 import BeautifulSoup
 from scraping import scrape_dopeology_incidents
 from scraping import scrape_incident
-from requests_html import HTMLSession # to remove
+import os
 import pandas as pd
 import numpy as np
 
 
 def scrape_adrv():
 
-    incidents = scrape_dopeology_incidents()
+    incidents = scrape_dopeology_incidents().tail(1314-1100)
 
     df = pd.DataFrame()
 
+    i= 1101
     for incident in incidents.itertuples():
+        try:
+            print(i)
+            i+= 1
 
-        url = getattr(incident, 'incident_url')
+            url = getattr(incident, 'incident_url')
 
-        print(url)
+            print(url)
 
-        people = scrape_incident(url)
-        if people is None:
+            people = scrape_incident(url)
+            if people is None:
+                continue
+            incident = pd.DataFrame([incident])
+            incident = pd.concat([incident]*people.shape[0], axis =0)
+
+            incident_df = pd.concat([people, incident], axis =1, ignore_index= True)
+            df = pd.concat([df, incident_df], axis = 0)
+        except Exception as e:
+            print(e)
             continue
-        incident = pd.DataFrame([incident])
-        incident = pd.concat([incident]*people.shape[0], axis =0)
 
-        incident_df = pd.concat([people, incident], axis =1, ignore_index= True)
-        df = pd.concat([df, incident_df], axis = 0)
+        if i % 100 == 0:
+            df.to_csv('dopeology_{}.csv'.format(i))
 
-    df.rename(columns=['name', 'team', 'role', 'to_drop', 'incident_name', 'incident_url',
-                       'incident_type', 'incident_date'])
 
     df.to_csv('dopeology.csv')
     return df
 
 
-scrape_adrv()
+#scrape_adrv()
+
+
